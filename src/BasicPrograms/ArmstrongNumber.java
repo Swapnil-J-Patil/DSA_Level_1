1f1c0f883f425859782ee9d815588f6245e5f06a
Swapnil_Patil <swapnilp0501@gmail.com>
2024-12-14 16:51:12 +0530

    fix: changed package name to BasicPrograms

--- src/BasicPrograms/ArmstrongNumber.java
+++ src/BasicPrograms/ArmstrongNumber.java
@@ -1,4 +1,4 @@
-package Basics;
+package BasicPrograms;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
--- src/BasicPrograms/PrimeNumber.java
+++ src/BasicPrograms/PrimeNumber.java
@@ -1,4 +1,4 @@
-package Basics;
+package BasicPrograms;
 
 public class PrimeNumber {
     public static void main(String[] args) {
