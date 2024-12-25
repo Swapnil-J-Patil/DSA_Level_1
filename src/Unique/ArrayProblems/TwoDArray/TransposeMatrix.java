7430f9eebbf94c184c5cc8983eda92f887803df5
Swapnil <swapnilp0501@gmail.com>
2024-12-25 09:49:37 +0530

    ci: Added new array problems

--- src/Unique/ArrayProblems/TwoDArray/TransposeMatrix.java
+++ src/Unique/ArrayProblems/TwoDArray/TransposeMatrix.java
@@ -15,7 +15,7 @@ public class TransposeMatrix {
         int[][] ans=transpose(arr);
         for (int i = 0; i < ans[0].length; i++) {
             for (int j = 0; j < ans.length; j++) {
-                System.out.print(ans[i][j]);
+                System.out.print(ans[i][j] +" ");
             }
             System.out.println();
         }
