401d59a84828c24c96dc39374e7f2b16e05d835a
swapnil_patil333 <swapnil.patil@factsonline.in>
2024-12-25 09:45:20 +0530

    ci: Some changes done in transpose question

--- src/Unique/ArrayProblems/TwoDArray/TransposeMatrix.java
+++ src/Unique/ArrayProblems/TwoDArray/TransposeMatrix.java
@@ -1,13 +1,26 @@
 package Unique.ArrayProblems.TwoDArray;
 
+import java.util.Arrays;
+
 //867. Transpose Matrix
 //Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 //Output: [[1,4,7],[2,5,8],[3,6,9]]
 public class TransposeMatrix {
     public static void main(String[] args) {
-
+        int[][] arr={
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+        int[][] ans=transpose(arr);
+        for (int i = 0; i < ans[0].length; i++) {
+            for (int j = 0; j < ans.length; j++) {
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
     }
-    public int[][] transpose(int[][] matrix) {
+    public static int[][] transpose(int[][] matrix) {
 
         int row= matrix.length;
         int col= matrix[0].length;
