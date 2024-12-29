0a858388e2643a8abc16b41dfc453fdb8d43524a
Swapnil_Patil <swapnilp0501@gmail.com>
2024-12-29 14:08:15 +0530

    feat: Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//1480. Running Sum of 1d Array
-/*
-Input: nums = [1,2,3,4]
-        Output: [1,3,6,10]
-        Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].*/
-
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(isPrime(4));
     }
-
-    static boolean isPrime(int num)
-    {
-        if(num==2)
-        {
-            return true;
-        }
-        int div=2;
-        while (div*div < num)
-        {
-            if(num%div ==0)
-            {
-                return false;
-            }
-            div++;
-        }
-        return div*div>=num;
-    }
-
 }
