76a3b16c233822fbe760674f900b234af04bc747
Swapnil_Patil <swapnilp0501@gmail.com>
2025-01-07 17:21:41 +0530

    feat: Added maximumSubArray problem

---
+++ src/ArrayProblems/LeetcodeProblems/MaximumSubArray.kt
@@ -0,0 +1,11 @@
+package ArrayProblems.LeetcodeProblems
+
+//53. Maximum Subarray
+/*Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.*/
+class MaximumSubArray {
+
+}
