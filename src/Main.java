39fd269f26be595f3811d3457e5ca9a182be8343
Swapnil_Patil <swapnilp0501@gmail.com>
2025-01-26 19:17:29 +0530

    refactor: updated Main.java question

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 
 //1431. Kids With the Greatest Number of Candies
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
     }
+    public static int[] twoSumOptimized(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target-nums[i]))
+            {
+                return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[] {1,2};
+    }
+
 }
