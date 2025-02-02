ba41dbaf83bc407b355a3e66b55f773f73a067cf
Swapnil_Patil <swapnilp0501@gmail.com>
2025-02-02 19:51:50 +0530

    refactor: updated Main.java file for practice.

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,20 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr={1,2,3};
+        System.out.println(minCostToMoveChips(arr));
     }
+    public static int minCostToMoveChips(int[] position) {
+        int evenCount = 0;
+        int oddCount = 0;
 
-
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        return Math.min(oddCount, evenCount);
+    }
 }
