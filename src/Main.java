db578f2885b53f956b5a7cd54d2d27e31437b39e
Swapnil <swapnilp0501@gmail.com>
2025-04-17 13:31:01 +0530

    feat: changed main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,30 @@
 import java.util.*;
 
 
-/*Input: n = 5
-        Output: [-7,-1,1,3,4]
-        Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].*/
+
+/*Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234*/
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(sumZero(5)));
+        int[] arr={1,2,0,0};
+        //System.out.println(addToArrayForm(arr,34));
+        System.out.println(addToArrayOptimized(arr,39));
     }
-    public static int[] sumZero(int n) {
-        int[] ans=new int[n];
-        int sum=0;
-        for (int i = 1; i < n; i++) {
-            ans[i]=i;
-            sum+=ans[i];
+    public static List<Integer> addToArrayOptimized(int[] num, int k)
+    {
+        List<Integer> ans=new LinkedList<>();
+        int i= num.length-1;
+        while (i >= 0 || k>0)
+        {
+            if(i>=0)
+            {
+                k+=num[i--];
+            }
+            ans.addFirst(k%10);
+            k/=10;
         }
-        int firstDigit= -1 * sum;
-        ans[0]=firstDigit;
         return ans;
     }
 }
