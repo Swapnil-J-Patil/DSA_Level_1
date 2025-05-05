a355b14ce587f041f216c70b6db9592c66e52d37
Swapnil <swapnilp0501@gmail.com>
2025-05-05 16:58:16 +0530

    refactor: Updated ArmstrongNumber.java

--- src/BasicPrograms/ArmstrongNumber.java
+++ src/BasicPrograms/ArmstrongNumber.java
@@ -6,14 +6,26 @@ public class ArmstrongNumber {
     }
     static boolean isArmstrong(int num)
     {
-        int original=num; //We are changing the num over the time that's why keep the original value stored beforehand
+        //Store the original number beforehand
+        int original=num;
+
+        //Initialize sum to 0
         int sum=0;
+
+        //Rum the while loop till the number is greater than 0
         while (num>0)
         {
+            //get the remainder (num % 10)
             int rem=num%10;
+
+            // Add the 3 times product of rem to the existing sum
             sum+=rem*rem*rem;
+
+            //Divide the num with 10
             num/=10;
         }
+
+        //return the value of (original==sum)
         return original==sum;
     }
 }
