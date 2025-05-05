da0a3efffa784860ba3ef8ae7002fdfd21b0a2a0
Swapnil <swapnilp0501@gmail.com>
2025-05-05 16:42:06 +0530

    refactor: Updated PrimeNumber.java

--- src/BasicPrograms/PrimeNumber.java
+++ src/BasicPrograms/PrimeNumber.java
@@ -6,20 +6,31 @@ public class PrimeNumber {
     }
     static boolean isPrime(int num)
     {
+        //check first if the number is 2 or not
         if(num==2)
         {
             return true;
         }
+
+        //Set the divisor as 2
         int div=2;
+
+        //Run the while loop till the square of divisor becomes less than num
+
         while (div * div <num)
         {
+            //if the value of num % div == 0 then return value as false
             if (num % div==0)
             {
                 return false;
             }
+
+            //increase value of divisor by 1
             div++;
         }
 
-        return div*div >= num; //After loop ends check if the div square is greater than the number
+        //After loop ends return the value of ( div square is greater than the number )
+
+        return div*div >= num;
     }
 }
