f683795fc06e7deaea786f96708ad9aa8d5cc87b
Swapnil <swapnilp0501@gmail.com>
2025-06-03 17:05:58 +0530

    feat: Added CeilingOfNumber code

---
+++ src/ArrayProblems/BinarySearch/CeilingOfNumber.java
@@ -0,0 +1,52 @@
+package ArrayProblems.BinarySearch;
+
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] arr={1,3,4,5,6,8};
+        System.out.println(ceilingNum(arr,7));
+        System.out.println(floorNum(arr,7));
+
+    }
+    static int ceilingNum(int[] arr,int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end)
+        {
+            int mid=start + (end-start)/2;
+            if(target> arr[mid])
+            {
+                start=mid+1;
+            }
+            else if(target<arr[mid])
+            {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return start;
+    }
+    static int floorNum(int[] arr,int target)
+    {
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end)
+        {
+            int mid=start + (end-start)/2;
+            if(target> arr[mid])
+            {
+                start=mid+1;
+            }
+            else if(target<arr[mid])
+            {
+                end=mid-1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
