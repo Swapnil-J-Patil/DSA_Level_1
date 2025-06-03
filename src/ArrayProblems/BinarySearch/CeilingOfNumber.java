ab95578af26920cbff09d21637ceb7fee9f2bed8
Swapnil <swapnilp0501@gmail.com>
2025-06-03 17:34:34 +0530

    feat: Added SmallestLetterGTET.java code

--- src/ArrayProblems/BinarySearch/CeilingOfNumber.java
+++ src/ArrayProblems/BinarySearch/CeilingOfNumber.java
@@ -3,14 +3,22 @@ package ArrayProblems.BinarySearch;
 public class CeilingOfNumber {
     public static void main(String[] args) {
         int[] arr={1,3,4,5,6,8};
-        System.out.println(ceilingNum(arr,7));
-        System.out.println(floorNum(arr,7));
+        System.out.println(ceilingNum(arr,0));
+        System.out.println(floorNum(arr,0));
 
     }
     static int ceilingNum(int[] arr,int target)
     {
+
         int start=0;
         int end=arr.length-1;
+
+        //if the target is not in the array
+        if(target> arr[end])
+        {
+            return -1;
+        }
+
         while (start<=end)
         {
             int mid=start + (end-start)/2;
@@ -18,7 +26,7 @@ public class CeilingOfNumber {
             {
                 start=mid+1;
             }
-            else if(target<arr[mid])
+            else if(target< arr[mid])
             {
                 end=mid-1;
             }
@@ -32,6 +40,13 @@ public class CeilingOfNumber {
     {
         int start=0;
         int end=arr.length-1;
+
+        //if the target is not in the array
+        if(target> arr[end])
+        {
+            return -1;
+        }
+
         while (start<=end)
         {
             int mid=start + (end-start)/2;
---
+++ src/ArrayProblems/BinarySearch/SmallestLetterGTET.java
@@ -0,0 +1,32 @@
+package ArrayProblems.BinarySearch;
+
+//744. Find Smallest Letter Greater Than Target
+public class SmallestLetterGTET {
+    public static void main(String[] args) {
+        char[] arr={'c','f','j'};
+        System.out.println(smallestLetter(arr,'k'));
+    }
+    static char smallestLetter(char[] arr, char target)
+    {
+        int start=0;
+        int end=arr.length -1;
+        int length=arr.length;
+        if(target >arr[end])
+        {
+            return arr[0];
+        }
+        while (start<=end)
+        {
+            int mid=start + (end -start)/2;
+            if(target<arr[mid])
+            {
+                end=mid -1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        return arr[start % length];
+    }
+}
