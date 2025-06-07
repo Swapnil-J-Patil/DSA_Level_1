3a85a434afe7d6bd2ecdcd5888ab43d14c41ade9
Swapnil_Patil <swapnilp0501@gmail.com>
2025-06-07 19:56:51 +0530

    feat: Added SearchInRotatedSortedArray.java

---
+++ src/ArrayProblems/BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,76 @@
+package ArrayProblems.BinarySearch;
+
+//33. Search in Rotated Sorted Array
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] arr={4,5,6,7,0,1,2};
+        System.out.println(search(arr,6));
+    }
+    static int search(int[] arr,int target)
+    {
+        int peak=peakOfArray(arr);
+        int left=binarySearch(arr,target,0,peak);
+        if(left!=-1)
+        {
+            return left;
+        }
+
+        return binarySearch(arr,target,peak+1,arr.length-1);
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end)
+    {
+        while (start<=end)
+        {
+            int mid=start +(end-start)/2;
+            if(target>arr[mid])
+            {
+                start=mid+1;
+            } else if (target<arr[mid]) {
+                end=mid-1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    static int peakOfArray(int[] arr)
+    {
+        int start=0;
+        int end=arr.length-1;
+
+        while (start<end)
+        {
+            int mid=start + (end-start)/2;
+
+            //Case1: 0,5,6,1,2 (mid: 6) mid > mid+1
+            //Here we already got the largest element so return it
+            //suppose mid+1 exceeds the size of the array
+            if(mid<end && arr[mid]>arr[mid+1])
+            {
+                return mid;
+            }
+
+            //Case2: 2,3,4,5,1 (mid: 4) mid < mid -1
+            //Here we already got the largest element so return it
+            //suppose there is no element for mid -1
+            if( mid > start && arr[mid] < arr[mid-1])
+            {
+                return mid-1;
+            }
+            //Case3: 5,1,3 (mid: 1) mid <= start
+            //Largest element lies on the left side
+            if (arr[mid] <=arr[start]) {
+                end=mid-1;
+            }
+            //Case4: 1,2,3,4,5 (mid: 3) mid > start
+            //Largest element lies on the right side
+            else {
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
