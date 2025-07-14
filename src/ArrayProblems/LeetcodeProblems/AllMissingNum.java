7b3c2b74452260602e1cfeb56e2880cebd0b21ab
Swapnil <swapnilp0501@gmail.com>
2025-07-14 13:01:21 +0530

    feat: Added AllMissingNum.java code

---
+++ src/ArrayProblems/LeetcodeProblems/AllMissingNum.java
@@ -0,0 +1,40 @@
+package ArrayProblems.LeetcodeProblems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+public class AllMissingNum {
+    public static void main(String[] args) {
+        int[] arr={4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(arr));
+    }
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int index=0;
+        List<Integer> ans=new ArrayList<>();
+        while (index< nums.length)
+        {
+            int correctIndex=nums[index]-1;
+            if(nums[index]!=correctIndex && index< nums.length && nums[correctIndex]!=nums[index])
+            {
+                swap(nums,index,correctIndex);
+            }
+            else {
+                index++;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(i!=nums[i]-1)
+            {
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+    public static void swap(int[] arr, int first, int second)
+    {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
--- src/ArrayProblems/LeetcodeProblems/MissingNum.java
+++ src/ArrayProblems/LeetcodeProblems/MissingNum.java
@@ -3,7 +3,7 @@ package ArrayProblems.LeetcodeProblems;
 //https://leetcode.com/problems/missing-number/description/
 public class MissingNum {
     public static void main(String[] args) {
-        int[] arr={3,4,2,1,0};
+        int[] arr={3,1,0};
         System.out.println(missingNum(arr));
     }
     static int missingNum(int[] arr)
@@ -14,6 +14,7 @@ public class MissingNum {
             //As the range is from [0,n] we are not doing -1
             int correctIndex=arr[index];
             //Skip the missing number
+            //As we are using [0,n] if the array size is 3 then the index will go out of bounds for n[3] ---{3,1,0}
             if(arr[index]< arr.length && arr[index]!=arr[correctIndex])
             {
                 swap(arr,index,correctIndex);
