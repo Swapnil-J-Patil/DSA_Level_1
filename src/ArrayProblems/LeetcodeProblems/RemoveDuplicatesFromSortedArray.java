ae5a2ed48a2517db1bee6ec2582bfcfb1ea88b88
Swapnil <swapnilp0501@gmail.com>
2026-06-05 17:42:58 +0530

    feat: Added removeDuplicate code

--- src/ArrayProblems/LeetcodeProblems/RemoveDuplicatesFromSortedArray.java
+++ src/ArrayProblems/LeetcodeProblems/RemoveDuplicatesFromSortedArray.java
@@ -19,6 +19,7 @@ public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
     int[] arr={0,0,1,1,1,2,2,3,3,4};
         System.out.println(removeDuplicates(arr));
+        System.out.println(Arrays.toString(removeDupli(arr)));
     }
 
     public static int removeDuplicates(int[] nums) {
@@ -30,5 +31,17 @@ public class RemoveDuplicatesFromSortedArray {
             }
         }
         return j;
+    }
+    public static int[] removeDupli(int[] arr)
+    {
+        int count=0;
+        for (int i = 1; i < arr.length; i++) {
+
+            if(arr[i]!= arr[count])
+            {
+                arr[++count]=arr[i];
+            }
+        }
+        return Arrays.copyOf(arr, count + 1);
     }
 }
